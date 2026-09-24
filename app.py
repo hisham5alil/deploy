704e2ba8c80c8f8991888515f2131be989b00671
Hisham Khalil <hishamkhalil44@gmail.com>
2026-09-24 18:02:08 +0300

    streamlit app

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+
+file_name = 'model.pkl'
+with open(file_name, 'rb') as file:
+    model = pickle.load(file)
+
+
+st.title("Classifier Model")
+st.header('Hello')
+
+if st.button('button'):
+    st.write('This is a button')
+    age = st.sidebar()
+
+# st.sidebar()
